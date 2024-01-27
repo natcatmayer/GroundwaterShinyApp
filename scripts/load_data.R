98d3cc2344a1698598d158e9b157b8ec2c7f7c70
chelseaasanford <chelseaasanford@bren.ucsb.edu>
2024-01-27 12:51:57 -0800

    Read in Cal Enviro data.

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -9,6 +9,9 @@ pfos <- read_tsv(here('data', 'statewide_pfos_data.txt'))
 dwr <- read_tsv(here('data', 'dwr_water_quality_data.txt'))
 depth <- read_tsv(here('data', 'depth_to_water_and_groundwater_elevation_data.txt'))
 
+enviro <- read_csv(here('data', 'SB353_tract.csv'))
+enviro_all <- read_csv(here('data', 'SB353_tract_all.csv'))
+tribal <- read_csv(here('data', 'SB353_tribal_area.csv'))
 
 
 

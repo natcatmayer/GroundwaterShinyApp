2e6ee031538d4bc3cfcc479a4e1a07dfdad842dc
natcatmayer <nataliemayer@ucsb.edu>
2024-02-11 18:14:38 -0800

    remove redundant nitrate dataset clean and join dwr and pfos data to make water quality df

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -6,15 +6,49 @@ library(terra)
 library(tidyterra)
 library(tmap)
 
-# upload data 
+# load water data 
 
 pfos <- read_tsv(here('data', 'statewide_pfos_data.txt'))
 dwr <- read_tsv(here('data', 'dwr_water_quality_data.txt'))
 depth <- read_tsv(here('data', 'depth_to_water_and_groundwater_elevation_data.txt'))
 
+
+# clean pfos data 
+pfos_df <- pfos %>%
+  select(chemical = GM_CHEMICAL_NAME,
+         date = GM_SAMP_COLLECTION_DATE,
+         latitude = GM_LATITUDE, 
+         longitude = GM_LONGITUDE, 
+         measurement = GM_RESULT, 
+         units = GM_CHEMICAL_UNITS) %>%
+  filter(chemical ==  c("Perfluorooctanoic acid (PFOA)", "Perfluorooctane sulfonate (PFOS)"))
+
+
+# clean dwr data 
+dwr_df <- dwr %>%
+  select(chemical = GM_CHEMICAL_NAME, 
+         date = GM_SAMP_COLLECTION_DATE,
+         measurement = GM_RESULT,
+         units = GM_CHEMICAL_UNITS, 
+         latitude = GM_LATITUDE, 
+         longitude = GM_LONGITUDE) %>%
+  filter(chemical == c("Nitrate as N", 
+                       "Lead", 
+                       "Arsenic", 
+                       "pH", 
+                       "Mercury", 
+                       "bicarbonate HCO3"))
+
+# join dwr and pfos data 
+
+water_quality <- full_join(pfos_df, dwr_df, by = NULL)
+
+
+
 enviro <- read_csv(here('data', 'SB353_tract.csv'))
 enviro_all <- read_csv(here('data', 'SB353_tract_all.csv'))
 tribal <- read_csv(here('data', 'SB353_tribal_area.csv'))
+
 
 
 #### California Counties Data

d304bee8187e276f392297713e8fc7d411503cbb
natcatmayer <nataliemayer@ucsb.edu>
2024-02-11 17:53:57 -0800

    remove redundant nitrate data

--- scripts/load_data.R
+++ scripts/load_data.R
@@ -4,7 +4,6 @@ library(here)
 
 # upload data 
 
-nitrate <- read_tsv(here::here('data', 'ucdavis_nitrate_data.txt'))
 pfos <- read_tsv(here('data', 'statewide_pfos_data.txt'))
 dwr <- read_tsv(here('data', 'dwr_water_quality_data.txt'))
 depth <- read_tsv(here('data', 'depth_to_water_and_groundwater_elevation_data.txt'))

ccab8a6f4bfa29e1b3ba893826c38522dd80cfcf
madicalbert <madisoncalbert@bren.ucsb.edu>
2024-02-25 16:50:22 -0800

    updated

--- scripts/Madi_Script.R
+++ scripts/Madi_Script.R
@@ -13,36 +13,57 @@ ui <- fluidPage(
     
     tabPanel( ### start tab 1
       title = 'Home',
-      
-      img(src = 'devereaux_slough.png', height = 140, width = 400),
+    
+      img(src = "rstudio.png", height = 140, width = 400),
       p('Photo caption'),
       hr(), 
       
       fluidRow( # start fluid row 1.1
+        
         column(width = 9,
+               h2(strong("Purpose")),
+               helpText("Groundwater in California is rapidly dwindling. We aim to explore ....."),
                
-               textInput(" ", label = h3("Purpose"),
-              
-               )
-        ), ### end column
+               h2(strong("Background")),
+               helpText("Groundwater is important because....."),
+               
+               h2(strong("Website Content")),
+               helpText(" Tab 1: Groundwater Levels
+                        Tab 2: Gwater Quality 
+                        Tab 3: EJ
+                        Tab 4 Ecosystems"),
+               
+               h2(strong("Data Summary")),
+               helpText("In this analysis, ......"),
+               
+               h2(strong("Data Source")),
+               helpText("Data Sourced from blah blah blah...."),
+               
+        ), ### end column 
+        
         column(width = 3,
-               h3('Map Here'),
-               #plotOutput('insert_map')
-        )
+               h3(''),
+               tags$figure(
+                 class = "centerFigure",
+                 tags$img(
+                   src = "cts.jpg",
+                   width = 600,
+                   alt = "California tiger salamander (CTS)"
+                 ),
+                 tags$figcaption("California tiger salamander (CTS)"))
+        ) ### end column 
       ), ### end fluidRow 1.1
       
+      
+      
       hr(), ### horizontal rule so the row breaks are easier to see
-      p('talk about how they should look at the map'),
+      p('Developed by Natalie Mayer, Chelsea Sanford, and Madi Calbert'),
       hr()
       
       
     ) ### end tab 1
   ) ### end tabsetPanel
   ) # end Fluidpage
-
-
-
-
 
 
 

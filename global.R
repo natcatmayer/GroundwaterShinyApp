52e012e077f065a0afa895568b7354863c9a1f56
natcatmayer <nataliemayer@ucsb.edu>
2024-03-15 15:40:37 -0700

    trying to deploy

--- global.R
+++ global.R
@@ -14,8 +14,6 @@ library(fable)
 
 ### Data
 
-water_quality <- read_csv(here('data', 'water_quality.csv'))
-depth_df <- read_csv(here('data', 'groundwater_depth.csv'))
 ca_counties_raw_sf <- read_sf(here("data/ca_counties/CA_Counties_TIGER2016.shp"))
 socioeco_data <- read.csv(here('data/socioeco.csv'))
 
@@ -23,44 +21,6 @@ ca_counties_sf <- ca_counties_raw_sf %>%
   janitor::clean_names() %>%
   mutate(land_km2 = aland / 1e6) %>%
   select(county = name, land_km2)
-
-
-# convert water quality to sf 
-water_quality_sf <- st_as_sf(x = water_quality, 
-                             coords = c('longitude', 'latitude'), 
-                             crs = 4326)
-
-water_quality_sf <- st_transform(water_quality_sf, 3857)
-
-# combine water quality and county data 
-county_water <- st_join(ca_counties_sf, water_quality_sf)
-water_county <- st_join(water_quality_sf, ca_counties_sf)
-
-water_county <- water_county %>%
-  separate(date, c("year", "month", "day"))
-
-county_water <- county_water %>%
-  separate(date, c("year", "month", "day"))
-
-# convert depth to sf 
-depth_sf <- st_as_sf(x = depth_df, 
-                     coords = c('longitude', 'latitude'), 
-                     crs = 4326)
-
-depth_sf <- st_transform(depth_sf, 3857)
-
-# combine groundwater depth and ca counties 
-gw_county <- st_join(depth_sf, ca_counties_sf)
-county_gw <- st_join(ca_counties_sf, depth_sf)
-
-county_gw_avg <- county_gw %>%
-  drop_na() %>%
-  group_by(county, year) %>%
-  summarise(average_depth = mean(depth_to_water))
-
-water_county_avg <- county_water %>%
-  group_by(county, chemical, year) %>%
-  summarise(avg_measure = mean(measurement))
 
 ### Socioeconmic data wrangling 
 socio_by_county <- socioeco_data %>% 
@@ -104,11 +64,16 @@ county_socio_join <- merge(x = ca_counties_sf,
 ## tab 2 data load 
 
 
-quality_avg <- read_sf(dsn = here('data', 'avg_so_cal_water_quality.gpkg'))
+quality_avg_csv <- read_csv(here('data', 'quality_avg.csv'))
 depth_avg <- read_sf(dsn = here('data', 'depth_average.gpkg'))
 county_shapes <- read_sf(dsn = here('data', 'so_cal_county_shape.gpkg'))
 epa_levels <- read_csv(here('data', 'epa_levels.csv')) %>% janitor::clean_names()
 
+
+quality_avg <- ca_counties_sf %>%
+  left_join(quality_avg_csv, by = c('county' = 'name')) %>%
+  select(-land_km2) %>%
+  rename(name = county)
 
 ## predictions load data
 
